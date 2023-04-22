9940195a3b71ba2fc681664609f7c45cb56c5784
Billy Lin <billylin80125@gmail.com>
2023-04-21 18:39:47 -0700

    completed skeleton for freertos

---
+++ esp_src/driver/serial.h
@@ -0,0 +1,15 @@
+// // this is the header guard
+// #ifndef SERIAL_H
+// #define SERIAL_H
+
+// #include "stdint.h"
+
+// extern SoftwareSerial port;
+// extern const uint8_t numChars;
+// extern char receivedChars[];   // an array to store the received data
+
+// // contains includes of the serial reading
+// // contains the buffers of the serial buffer
+// // contains the function definition of the serial read function (should be called in the freeRTOS)
+// void tskSerialMain(); // a placeholder code
+// #endif
